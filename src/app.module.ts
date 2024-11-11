a1cb85253243e32734fe34bbe012b09cc599047a
Mitso9898 <162876584+Mitso9898@users.noreply.github.com>
2024-11-11 22:39:55 +0200

    create and register listing resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ListingModule } from './listing/listing.module';
 
 @Module({
-  imports: [],
+  imports: [ListingModule],
   controllers: [AppController],
   providers: [AppService],
 })
